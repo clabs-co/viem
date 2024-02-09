eac4bf70ff7e711900130c78359c14cd995fd99e
aaronmgdr <aaronmgdr@users.noreply.github.com>
2024-02-09 09:50:14 +0000

    chore: format

--- src/chains/celo/fees.test.ts
+++ src/chains/celo/fees.test.ts
@@ -1,31 +1,47 @@
 import { describe, expect, test } from 'vitest'
 import { getBlock } from '~viem/actions/public/getBlock.js'
-import { createPublicClient, http, type ChainEstimateFeesPerGasFnParameters } from '~viem/index.js'
+import {
+  http,
+  type ChainEstimateFeesPerGasFnParameters,
+  createPublicClient,
+} from '~viem/index.js'
 import { celo } from '../index.js'
-import { fees } from "./fees.js"
+import { fees } from './fees.js'
 import { formatters } from './formatters.js'
 
-const client = createPublicClient ({
+const client = createPublicClient({
   transport: http(),
-  chain: celo
+  chain: celo,
 })
 
-const randomness = {committed: '0x0000000000000000000000000000000000000000000000000000000000000000', revealed: '0x0000000000000000000000000000000000000000000000000000000000000000'} as const
+const randomness = {
+  committed:
+    '0x0000000000000000000000000000000000000000000000000000000000000000',
+  revealed:
+    '0x0000000000000000000000000000000000000000000000000000000000000000',
+} as const
 
-type ChainEstimateParams = ChainEstimateFeesPerGasFnParameters<typeof formatters>
+type ChainEstimateParams = ChainEstimateFeesPerGasFnParameters<
+  typeof formatters
+>
 
 const baseParams = {
-        client,
-        multiply: (x: bigint) => x * 102n / 100n,
-        type: 'eip1559'
-      } satisfies Partial<ChainEstimateParams>
+  client,
+  multiply: (x: bigint) => (x * 102n) / 100n,
+  type: 'eip1559',
+} satisfies Partial<ChainEstimateParams>
 
 describe('celo/fees', () => {
   describe('estimateFeesPerGas()', async () => {
     test('default', async () => {
       const block = await getBlockWithRandomness()
-      const params: ChainEstimateParams = {...baseParams, block,  request: { to: '0xto', value: 0n }}
-      const { maxFeePerGas, maxPriorityFeePerGas } = await fees.estimateFeesPerGas(params)
+      const params: ChainEstimateParams = {
+        ...baseParams,
+        block,
+        request: { to: '0xto', value: 0n },
+      }
+      const { maxFeePerGas, maxPriorityFeePerGas } =
+        await fees.estimateFeesPerGas(params)
       expect(maxFeePerGas).toBeDefined()
       expect(maxPriorityFeePerGas).toBeDefined()
     })
@@ -34,24 +50,21 @@ describe('celo/fees', () => {
       const block = await getBlockWithRandomness()
       const params: ChainEstimateParams = {
         ...baseParams,
-        block: {...block, randomness},
+        block: { ...block, randomness },
         request: {
-          feeCurrency: '0xfeeCurrency'
-        }
+          feeCurrency: '0xfeeCurrency',
+        },
       }
       expect(fees.estimateFeesPerGas(params)).toEqual({
         maxFeePerGas: 0,
-        maxPriorityFeePerGas: 0
+        maxPriorityFeePerGas: 0,
       })
     })
-
   })
 })
 
-
 async function getBlockWithRandomness() {
-  const block = await getBlock(client, {blockTag: 'pending'})
-  console.log("block", block)
-  return {...block, randomness}
+  const block = await getBlock(client, { blockTag: 'pending' })
+  console.log('block', block)
+  return { ...block, randomness }
 }
-
--- src/chains/celo/fees.ts
+++ src/chains/celo/fees.ts
@@ -1,41 +1,44 @@
-import { estimateFeesPerGas } from "~viem/actions/public/estimateFeesPerGas.js"
+import { estimateFeesPerGas } from '~viem/actions/public/estimateFeesPerGas.js'
 import type { Client } from '~viem/clients/createClient.js'
-import { type Address, type ChainFees, type Hex } from "~viem/index.js"
+import { type Address, type ChainFees, type Hex } from '~viem/index.js'
 
-import { formatters } from "./formatters.js"
-
+import { formatters } from './formatters.js'
 
 export const fees = {
   estimateFeesPerGas: async (params) => {
     // When using token to pay for fees, we need to estimate the fees in the value of the token
     if (params.request?.feeCurrency) {
-
-      const [ maxFeePerGas, maxPriorityFeePerGas] = await Promise.all([
-        estimateFeePerGasInFeeCurrency(params.client, params.request.feeCurrency),
-        estimateMaxPriorityFeePerGasInFeeCurrency(params.client, params.request.feeCurrency)
+      const [maxFeePerGas, maxPriorityFeePerGas] = await Promise.all([
+        estimateFeePerGasInFeeCurrency(
+          params.client,
+          params.request.feeCurrency,
+        ),
+        estimateMaxPriorityFeePerGasInFeeCurrency(
+          params.client,
+          params.request.feeCurrency,
+        ),
       ])
 
       return {
         maxFeePerGas,
-        maxPriorityFeePerGas
+        maxPriorityFeePerGas,
       }
     }
 
     return estimateFeesPerGas(params.client, params)
-  }
-
+  },
 } satisfies ChainFees<typeof formatters>
 
-
 type RequestGasPriceInFeeCurrencyParams = {
-  Method: 'eth_gasPrice';
-  Parameters: [Address];
-  ReturnType: Hex;
+  Method: 'eth_gasPrice'
+  Parameters: [Address]
+  ReturnType: Hex
 }
 
-
-
-async function estimateFeePerGasInFeeCurrency(client: Client, feeCurrency: Address) {
+async function estimateFeePerGasInFeeCurrency(
+  client: Client,
+  feeCurrency: Address,
+) {
   const fee = await client.request<RequestGasPriceInFeeCurrencyParams>({
     method: 'eth_gasPrice',
     params: [feeCurrency],
@@ -44,15 +47,19 @@ async function estimateFeePerGasInFeeCurrency(client: Client, feeCurrency: Addre
 }
 
 type RequestMaxGasPriceInFeeCurrencyParams = {
-  Method: 'eth_maxPriorityFeePerGas';
-  Parameters: [Address];
-  ReturnType: Hex;
+  Method: 'eth_maxPriorityFeePerGas'
+  Parameters: [Address]
+  ReturnType: Hex
 }
 
-async function estimateMaxPriorityFeePerGasInFeeCurrency(client: Client, feeCurrency: Address) {
-  const feesPerGas = await client.request<RequestMaxGasPriceInFeeCurrencyParams>({
-    method: 'eth_maxPriorityFeePerGas',
-    params: [feeCurrency],
-  })
+async function estimateMaxPriorityFeePerGasInFeeCurrency(
+  client: Client,
+  feeCurrency: Address,
+) {
+  const feesPerGas =
+    await client.request<RequestMaxGasPriceInFeeCurrencyParams>({
+      method: 'eth_maxPriorityFeePerGas',
+      params: [feeCurrency],
+    })
   return BigInt(feesPerGas)
 }
