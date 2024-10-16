4264034964b93c08e4ae72f3c05b6617a5f993ea
Piers Powlesland <pierspowlesland@gmail.com>
2024-10-16 16:21:52 +0100

    Update test to check both fee calculation paths

--- src/celo/fees.test.ts
+++ src/celo/fees.test.ts
@@ -26,30 +26,37 @@ describe('celo/fees', () => {
     expect(requestMock).not.toHaveBeenCalled()
   })
 
-  test('calls the client when feeCurrency is provided', async () => {
+  test('calls the client when feeCurrency is provided celo L1', async () => {
     const requestMock = vi.spyOn(client, 'request')
 
 		const baseFee = 15057755162n
 		const priorityFee = 602286n
 
+    expect(celo.fees.estimateFeesPerGas).toBeTypeOf('function')
+
+		// The check to determine if the chain is L1 or L2 is done by checking to
+		// see if there is code at the proxy admin address (by calling
+		// eth_getCode), if there is code then the chain is considered to be L2. A
+		// response of '0x' as used here is what is returned when there is no code
+		// at the address.
+
     // @ts-ignore
     requestMock.mockImplementation((request) => {
       if (request.method === 'eth_gasPrice') return baseFee.toString()
       if (request.method === 'eth_maxPriorityFeePerGas') return priorityFee.toString()
+      if (request.method === 'eth_getCode') return '0x'
       return
     })
 
-    expect(celo.fees.estimateFeesPerGas).toBeTypeOf('function')
-
     const fees = await celoestimateFeesPerGasFn({
       client,
+			multiply: (value: bigint) => (value * 150n) / 100n,
       request: {
         feeCurrency: '0xfee',
       },
     } as any)
 
-    // The fees maxFeePerGas is calculated as (baseFee * multiplier) + maxPriorityFeePerGas
-    // Which is ((15057755162-602286) * 150 / 100) + 602286 = 22586331600
+    // For celo L1 the fees maxFeePerGas is calculated as `baseFee + maxPriorityFeePerGas`, the multiply method is not used.
     expect(fees).toMatchInlineSnapshot(`
         {
           "maxFeePerGas": ${baseFee + priorityFee}n,
@@ -64,5 +71,55 @@ describe('celo/fees', () => {
       method: 'eth_gasPrice',
       params: ['0xfee'],
     })
-  })
+	})
+
+  test('calls the client when feeCurrency is provided celo L2', async () => {
+    const requestMock = vi.spyOn(client, 'request')
+
+		const baseFee = 15057755162n
+		const priorityFee = 602286n
+
+    expect(celo.fees.estimateFeesPerGas).toBeTypeOf('function')
+
+		// The check to determine if the chain is L1 or L2 is done by checking to
+		// see if there is code at the proxy admin address (by calling
+		// eth_getCode), if there is code then the chain is considered to be L2. A
+		// response longer than '0x' as used here is what is returned when there is
+		// code at the address.
+		
+    // @ts-ignore
+    requestMock.mockImplementation((request) => {
+      if (request.method === 'eth_gasPrice') return (baseFee + priorityFee).toString()
+      if (request.method === 'eth_maxPriorityFeePerGas') return priorityFee.toString()
+      if (request.method === 'eth_getCode') return '0x00400400404040404040404' 
+      return
+    })
+
+		const multiply = (value: bigint) => (value * 150n) / 100n
+    const feesCeloL1 = await celoestimateFeesPerGasFn({
+      client,
+			multiply: multiply,
+      request: {
+        feeCurrency: '0xfee',
+      },
+    } as any)
+
+		// For Celo L2 the fees maxFeePerGas is calculated as the following where
+		// multiply is the method passed to celoestimateFeesPerGasFn:
+		//    `multiply(baseFeePerGas - maxPriorityFeePerGas) + maxPriorityFeePerGas`.
+    expect(feesCeloL1).toMatchInlineSnapshot(`
+        {
+          "maxFeePerGas": ${multiply(baseFee) + priorityFee}n,
+          "maxPriorityFeePerGas": ${priorityFee}n,
+        }
+      `)
+    expect(requestMock).toHaveBeenCalledWith({
+      method: 'eth_maxPriorityFeePerGas',
+      params: ['0xfee'],
+    })
+    expect(requestMock).toHaveBeenCalledWith({
+      method: 'eth_gasPrice',
+      params: ['0xfee'],
+    })
+	})
 })
